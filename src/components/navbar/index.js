5bcece8c6a3f6b2f170698662eeb9984e02301c3
Breno Menezes <brenoma@live.com>
2020-08-24 18:33:05 -0300

    Added Dynamic menu using dispatch

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Navbar() {
+    const dispatch = useDispatch();
+
     return (
         <nav className="navbar navbar-expand-lg">
             <span className="navbar-brand text-white font-weight-bold">Eventos</span>
@@ -13,8 +16,21 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/register">Cadastrar</Link></li>
-                    <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+
+                    {
+                        useSelector(state => state.userLogged) > 0 ?
+                            <>
+                                <li className="nav-item"><Link className="nav-link" to="">Publicar Evento</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="">Meus Eventos</Link></li>
+                                <li className="nav-item"><Link className="nav-link" onClick={() => dispatch({type: 'LOG_OUT'})}>Sair</Link></li>
+                            </>
+                            :
+                            <>
+                                <li className="nav-item"><Link className="nav-link" to="/register">Cadastrar</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                            </>
+
+                    }
                 </ul>
                 {/* <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
