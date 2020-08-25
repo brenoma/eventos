b68273de7f46c7813aabef27c1e1e8dddf9dc223
Breno Menezes <brenoma@live.com>
2020-08-25 13:57:51 -0300

    Added search filter by name with case insensitive

--- src/components/event/index.js
+++ src/components/event/index.js
@@ -1,28 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import firebase from '../../config/firebase';
 
 import './event.css';
 
-function Event() {
+function Event({ id, img, title, description, views }) {
+
+    const [urlImg, setUrlImg] = useState();
+
+    useEffect(() => {
+        firebase.storage().ref(`images/${img}`).getDownloadURL().then(url => setUrlImg(url));
+    }, [urlImg]);
+
     return (
         <div className="col-md-3 col-sm-12">
-            <img src="https://via.placeholder.com/500" className="card-img-top img-card" alt="Imagem do Evento" />
+            <img src={urlImg} className="card-img-top img-card" alt="Imagem do Evento" />
 
             <div className="card-body">
-                <h5>Título do Evento</h5>
+                <h5>{title}</h5>
                 <p className="card-text text-justify">
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    {description}
                 </p>
 
                 <div className="row footer-card d-flex align-items-center">
 
                     <div classname="col-6">
-                        <Link to="/" className="btn btn-sm btn-details">+ Detalhes</Link>
+                        <Link to={"/eventdetail/" + id} className="btn btn-sm btn-details">+ Detalhes</Link>
                     </div>
 
                     <div className="col-6 text-right">
-                        <i class="fas fa-eye"></i><span className="ml-1">1337</span>
+                        <i class="fas fa-eye"></i><span className="ml-1">{views}</span>
                     </div>
                 </div>
             </div>
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import firebase from '../../config/firebase';
 
 import Navbar from '../../components/navbar';
 import Event from '../../components/event';
@@ -8,18 +9,49 @@ import Event from '../../components/event';
 import './home.css';
 
 function Home() {
+
+    const [events, setEvents] = useState([]);
+    const [search, setSearch] = useState('');
+    let listEvents = [];
+
+    useEffect(() => {
+        firebase.firestore().collection('events').get().then(async (result) => {
+            await result.docs.forEach(doc => {
+                if (doc.data().title.toUpperCase().indexOf(search.toUpperCase()) >= 0) {
+                    listEvents.push({
+                        id: doc.id,
+                        ...doc.data()
+                    });
+                }
+            })
+
+            setEvents(listEvents);
+        })
+    })
+
     return (
         <>
             <Navbar />
-            <h1>{useSelector(state => state.userMail)}</h1>
-            <h1>{useSelector(state => state.userLogged)}</h1>
+            {/* <h1>{useSelector(state => state.userMail)}</h1>
+            <h1>{useSelector(state => state.userLogged)}</h1> */}
 
-            <div className="row mx-1">
-                <Event />
-                <Event />
-                <Event />
-                <Event />
-                <Event />
+            <div className="row p-5">
+                <input
+                    onChange={(e) => setSearch(e.target.value)}
+                    type="text"
+                    className="form-control text-center"
+                    placeholder="Pesquisar evento pelo título"
+                />
+            </div>
+
+            <div className="row p-3 mx-1">
+                {events.map(item => <Event
+                    id={item.id}
+                    img={item.image}
+                    title={item.title}
+                    description={item.description}
+                    views={item.views}
+                />)}
             </div>
         </>
     )
